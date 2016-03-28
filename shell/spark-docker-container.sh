0e3fd4d104660b87d52c3bf44841ef0cfb6e515f
wangguoliang <wangguoliang@wangguoliangdeMac-mini.local>
2016-03-28 11:38:02 +0800

    remove some files

--- shell/spark-docker-container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Run a master container..."
-docker run --name master -it -p 8088:8088 -p 8042:8042 -p 8085:8080 -p 4040:4040 -p 7077:7077 -p 2022:22  -v /data:/data  -h master -d sequenceiq/spark:1.6.0
-
-echo "Run worker1 container..."
-docker run --name worker1 -it -h worker1 -d sequenceiq/spark:1.6.0
-
-echo "Run worker2 container..."
-docker run --name worker2 -it -h worker2 -d sequenceiq/spark:1.6.0
